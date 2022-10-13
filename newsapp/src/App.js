471f6d36a725d276679e0ff064512e77b86f24ec
luckkalpna <107942615+luckkalpna@users.noreply.github.com>
2022-10-13 18:32:15 +0530

    pages routing

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -3,11 +3,6 @@ import './App.css';
 import React, { Component } from 'react';
 import Navbar from './components/Navbar'
 import News from './components/News'
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes
-} from "react-router-dom";
 
 export default class App extends Component {
   pageSize = 6;
--- newsapp/src/components/Navbar.js
+++ newsapp/src/components/Navbar.js
@@ -7,9 +7,9 @@ export class Navbar extends Component {
 			<div>
 				<nav className="navbar navbar-expand-lg bg-light">
 					<div className="container-fluid">
-						<a className="navbar-brand" href="/">
+						<Link className="navbar-brand" to="/">
 							NewsApp 
-						</a>
+						</Link>
 						<button
 							className="navbar-toggler"
 							type="button"
@@ -24,45 +24,45 @@ export class Navbar extends Component {
 						<div className="collapse navbar-collapse" id="navbarSupportedContent">
 							<ul className="navbar-nav me-auto mb-2 mb-lg-0">
 								<li className="nav-item">
-									<a className="nav-link active" aria-current="page" href="/">
-										Home
-									</a>
+									<Link className="nav-link active" aria-current="page" to="/">
+									Home
+									</Link>
 								</li>
 								<li className="nav-item">
-									<a className="nav-link" href="/business">
+									<Link className="nav-link" to="/business">
 									Business
-									</a>
+									</Link>
 								</li>
 								<li className="nav-item">
-									<a className="nav-link" href="/entertainment">
+									<Link className="nav-link" to="/entertainment">
 									Entertainment
-									</a>
+									</Link>
 								</li>
-								<li className="nav-item">
-									<a className="nav-link" href="/general">
+								{/* <li className="nav-item">
+									<Link className="nav-link" to="/general">
 									General
-									</a>
+									</Link>
 								</li>
 								<li className="nav-item">
-									<a className="nav-link" href="/health">
+									<Link className="nav-link" to="/health">
 									Health
-									</a>
+									</Link>
 								</li>
 								<li className="nav-item">
-									<a className="nav-link" href="/science">
+									<Link className="nav-link" to="/science">
 									Science
-									</a>
+									</Link>
 								</li>
 								<li className="nav-item">
-									<a className="nav-link" href="/sports">
+									<Link className="nav-link" to="/sports">
 									Sports
-									</a>
+									</Link>
 								</li>
 								<li className="nav-item">
-									<a className="nav-link" href="/technology">
+									<Link className="nav-link" to="/technology">
 									Technology
-									</a>
-								</li>
+									</Link>
+								</li> */}
 							</ul>
 						</div>
 					</div>
